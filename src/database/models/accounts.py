17cbe436fc21fb498717e063dd1282830f9bb323
pinigun <piniguun@yandex.ru>
2025-07-31 18:44:59 +0500

    Feature: add Account db model

--- src/database/models/accounts.py
+++ src/database/models/accounts.py
@@ -1,5 +1,13 @@
+from sqlalchemy import ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .base import Base 
 
 
 class Account(Base):
-    ...
+    __tablename__ = 'accounts'
+    
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id', ondelete='CASCADE'))
+    
+    user = relationship('User', back_populates='accounts')
+    transactions = relationship('Transaction', back_populates='account')
