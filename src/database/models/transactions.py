991c53790671e6530daa982300350df1770aec9c
pinigun <piniguun@yandex.ru>
2025-07-31 18:45:42 +0500

    Feature: add Transaction db model

--- src/database/models/transactions.py
+++ src/database/models/transactions.py
@@ -1,5 +1,14 @@
+from sqlalchemy import Float, Float, ForeignKey, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import UUID
 from .base import Base 
 
 
 class Transaction(Base):
-    ...
+    __tablename__ = 'transactions'
+    
+    id:         Mapped[str] = mapped_column(UUID, primary_key=True)
+    account_id: Mapped[int] = mapped_column(Integer, ForeignKey('accounts.id', ondelete="CASCADE"))
+    amount:     Mapped[float] = mapped_column(Float)
+    
+    account = relationship('Account', back_populates='transactions')
