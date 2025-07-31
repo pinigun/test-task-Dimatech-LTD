faaea633e361c543d80d3531ef06a4b0bce18ce7
pinigun <piniguun@yandex.ru>
2025-07-31 18:45:53 +0500

    Feature: add User db model

--- src/database/models/users.py
+++ src/database/models/users.py
@@ -4,7 +4,10 @@ from .base import Base
 
 
 class User(Base):
+    __tablename__ = 'users'
+    
     id:                 Mapped[int] = mapped_column(Integer, primary_key=True)
     full_name:          Mapped[str] = mapped_column(String(50), nullable=False)
     email:              Mapped[str] = mapped_column(String(100), nullable=False)
     hashed_password:    Mapped[str] = mapped_column(String, nullable=False)
+    
