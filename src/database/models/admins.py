d53c96f7f2a7634e6f772bd03ff1af034b46afc2
pinigun <piniguun@yandex.ru>
2025-07-31 18:46:11 +0500

    Feature: add Admin db model

--- src/database/models/admins.py
+++ src/database/models/admins.py
@@ -1,5 +1,12 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 from .base import Base 
 
 
 class Admin(Base):
-    ...
+    __tablename__ = 'admins'
+    
+    id:                 Mapped[int] = mapped_column(Integer, primary_key=True)
+    full_name:          Mapped[str] = mapped_column(String(50), nullable=False)
+    email:              Mapped[str] = mapped_column(String(50), nullable=False)
+    hashed_password:    Mapped[str] = mapped_column(String, nullable=False)
